8ad7704a6a45e924901a5e9318bd51813c076fba
Omniscient-Frame <satyam69180@gmail.com>
2026-08-04 18:48:03 +0530

    Problem-1A

--- Problem-1A.cpp
+++ Problem-1A.cpp
@@ -3,32 +3,6 @@
 #include<vector>
 #include<string>
 using namespace std;
-/*
-void multiplyNumericStrings(string num1,string num2) {
-    if (num1 == "0" || num2 == "0") cout<<"0"<<endl;;
-    
-    int n = num1.size();
-    int m = num2.size();
-    std::vector<int> result(n + m, 0);
-
-    for (int i = n - 1; i >= 0; i--) {
-        for (int j = m - 1; j >= 0; j--) {
-            int mul = (num1[i] - '0') * (num2[j] - '0');
-            int sum = mul + result[i + j + 1];
-
-            result[i + j + 1] = sum % 10;
-            result[i + j] += sum / 10;    
-        }
-    }
-
-    string final_str = "";
-    int i = 0;
-    while (i < result.size() && result[i] == 0) i++;
-    while (i < result.size()) final_str += to_string(result[i++]);
-
-    cout<<final_str<<endl;
-}
-*/
 
 int main(){
     long long m,n,a;
